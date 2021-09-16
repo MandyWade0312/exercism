0314a6198459b474bbfc9b0407a4f5ab5c59d5e6
MandyWade0312 <amanda.wade@kin.com>
2021-09-16 14:14:52 -0400

    Completed the scrabble exercise

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -7,26 +7,40 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class Scrabble
-  score = 0
-  letters = new.split('')
-  def score 
-    if letters == 'A' || 'E' || 'I' || 'O' || 'U' || 'L' || 'N' || 'R' || 'S' || 'T' 
-      score += 1
-    elsif letters == 'D' || 'G'
-      score += 2
-    elsif letters == 'B' || 'C' || 'M' || 'P'
-      score += 3
-    elsif letters == 'F' || 'H' || 'V' || 'W' || 'Y'
-      score += 4
-    elsif letters == 'K' 
-      score += 5
-    elsif letters == 'J' || 'X' 
-      score += 8
-    elsif letters == 'Q' || 'Z' 
-      score += 10
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def score
+    s_score = 0
+
+    if @word == nil
+      s_score = 0
     end
-    return score
+    
+    letters = @word.to_s.rstrip.upcase.split('')
+    letters.each do |letter|
+      if letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'L' || letter == 'N' ||              letter == 'R' || letter == 'S' || letter == 'T' 
+        s_score += 1
+      elsif letter == 'D' || letter == 'G'
+        s_score += 2
+      elsif letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P'
+        s_score += 3
+      elsif letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y'
+        s_score += 4
+      elsif letter == 'K' 
+        s_score += 5
+      elsif letter == 'J' || letter == 'X' 
+        s_score += 8
+      elsif letter == 'Q' || letter == 'Z' 
+        s_score += 10
+      end
+    end
+    return s_score
   end
 
- 
+  def self.score(word)
+    Scrabble.new(word).score
+  end
 end
